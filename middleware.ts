2b26d20b3ce8887caa672aa8602ac44ca05c5f90
gamcodev-josh <jfrank@gabelli.com>
2023-01-31 10:19:27 -0500

    updating middleware for cookies without typescript error

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,10 @@
-import { NextRequest, NextResponse } from "next/server";
+import { /* NextRequest, */ NextResponse } from "next/server";
 
-export async function middleware( req: NextRequest ) {
-	const { value: expirationDate } = req.cookies.get( 'fasttrack-expiration' )
-	const expired = expirationDate ? Date.now() > new Date( expirationDate ) - 432_000: true;					// 518,400 ms in six days
+export async function middleware( /* req: NextRequest */ ) {
+	// const { value: expirationDate } = req.cookies.get( 'fasttrack-expiration' )
+	// const expired = expirationDate ? Date.now() > new Date( expirationDate ) - 432_000: true;					// 518,400 ms in six days
 	const nextResponse = NextResponse.next()
-	if ( expired ) {
+	// if ( expired ) {
 		const fasttrackUrl = 'https://ftl.fasttrack.net/v1/auth/login'
 			+ '?account=' + process.env.NEXT_PUBLIC_FASTTRACKACCOUNT
 			+ '&pass=' + process.env.NEXT_PUBLIC_FASTTRACKPASSWORD
